89ca61e5ec9933b02b28fee4ac90c42f5bd48718
teddybear0430 <teddybear0430@hotmail.com>
2021-08-15 23:32:20 +0900

    feat: 横幅調整。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-9/12 mx-auto">
+    <div className="w-10/12 mx-auto lg:w-7/12">
       <Header />
       <Component {...pageProps} />
     </div>
