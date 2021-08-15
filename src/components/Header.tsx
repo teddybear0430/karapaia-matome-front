c6ea229f58069daebe5a5f71dffc4ccee1788792
teddybear0430 <teddybear0430@hotmail.com>
2021-08-15 23:32:08 +0900

    feat: 今日の日付表示。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import Link from 'next/link';
 
 const Header = () => {
+  const todayDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+
+    return `${year}年${month}月${date}日`.toString();
+  };
+
   return (
-    <header className="py-3">
+    <header className="flex items-center justify-between py-3 font-bold">
       <Link href="/">
-        <a className="text-2xl font-bold text-blue">カラパイアまとめ</a>
+        <a className="text-2xl text-blue">カラパイアまとめ</a>
       </Link>
+      <span>{todayDate()}</span>
     </header>
   );
 };
