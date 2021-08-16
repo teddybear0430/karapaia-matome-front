0f3b72fb60e98d04b117c1ad1b71aa038315553a
teddybear0430 <teddybear0430@hotmail.com>
2021-08-17 00:31:09 +0900

    feat: サイトタイトルのタグをh1に修正。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,11 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between py-3 font-bold">
-      <Link href="/">
-        <a className="text-2xl text-blue">カラパイアまとめ</a>
-      </Link>
+      <h1>
+        <Link href="/">
+          <a className="text-2xl text-blue">カラパイアまとめ</a>
+        </Link>
+      </h1>
       <span className="text-gray">{todayDate()}</span>
     </header>
   );
