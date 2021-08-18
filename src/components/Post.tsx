38ca4185289b728bed95a1c69f275736477937ff
teddybear0430 <teddybear0430@hotmail.com>
2021-08-18 23:21:03 +0900

    refactor: 記事を表示するコンポーネントのリファクタ。

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Post } from '../types/post';
 
-const PostItem: React.FC<{ post: Post }> = (postItem) => {
+const PostItem: React.FC<{ post: Post }> = ({ post }) => {
   return (
     <div className="my-3 border-b border-gray">
       <div className="flex justify-start mb-1 text-sm">
-        {postItem.post.archives.map((cat, i) => (
+        {post.archives.map((cat, i) => (
           <a key={i} href={cat.catUrl} className="mr-4 text-blue hover:underline hover:text-gray">
             {cat.catName}
           </a>
         ))}
       </div>
-      <a href={postItem.post.url} className="hover:underline text-blue">
-        {postItem.post.title}
+      <a href={post.url} className="hover:underline text-blue">
+        {post.title}
       </a>
       <div className="flex justify-start my-2 text-sm">
         <span>
           コメント数:
-          <span className={postItem.post.comment >= 50 ? 'text-red-600 font-bold' : ''}> {postItem.post.comment}</span>
+          <span className={post.comment >= 50 ? 'text-red-600 font-bold' : ''}> {post.comment}</span>
         </span>
-        <span className="ml-2">投稿日: {postItem.post.createdAt}</span>
+        <span className="ml-2">投稿日: {post.createdAt}</span>
       </div>
     </div>
   );
