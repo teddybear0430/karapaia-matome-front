e47091cb435a2b3587ae2c0dca3efa8ac987d201
teddybear0430 <teddybear0430@hotmail.com>
2021-09-18 20:03:56 +0900

    feat: 曜日表示して。

--- lib/utils/date-util.ts
+++ lib/utils/date-util.ts
@@ -6,8 +6,9 @@ export const dateUtil = () => {
     const year = today.getFullYear();
     const month = today.getMonth() + 1;
     const date = today.getDate();
+    const week = getWeekChars(`${year}/${month}/${date}`);
 
-    return `${year}年${month}月${date}日`.toString();
+    return `${year}年${month}月${date}日${week}`.toString();
   };
 
   const getYear = () => {
