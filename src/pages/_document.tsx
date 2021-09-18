58cab1f53f02374a0ef5c4f6db6c81474179aeda
teddybear0430 <teddybear0430@hotmail.com>
2021-09-18 19:02:25 +0900

    feat: GAの設定。

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,28 @@ class MyDocument extends Document {
   // }
 
   render() {
+    const GA_TRACKING_ID = 'G-KFMR91P7Z6';
+
     return (
       <Html>
         <Head>
           <meta name="description" content="カラパイアのまとめサイトです。" />
           <link rel="icon" href="/favicon.ico" />
+          {
+            // <!-- Global site tag (gtag.js) - Google Analytics -->
+          }
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+            `,
+            }}
+          />
         </Head>
         <body className="bg-white dark:bg-black transition-all duration-200">
           <Main />
